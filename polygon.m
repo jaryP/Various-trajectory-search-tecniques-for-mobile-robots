619a1656abff38bb557993ebaa234dd3f2fe99f9
jaryP <jarypomponi@gmail.com>
2017-06-19 16:09:40 +0200

    modificata funzione intersect ed eliminata funzione inside (accoccorpata ad intersect)

--- polygon.m
+++ polygon.m
@@ -11,20 +11,15 @@ classdef polygon
         end
         
         function [xi, yi] = intersect(obj,x,y)
-                        
+          
             [xp, yp] = polyxpoly(x,y,obj.x,obj.y, 'unique');
-            In = [[]; xp, yp];
-            ins=obj.inside(x,y);
-            if(numel(x(ins)) > 0 || numel(y(ins)) > 0)
-                In = [In; x(ins), y(ins)];
-            end
-            xi = In(:,1);
-            yi = In(:,2);
-            
+            xi = [[] xp];
+            yi = [[] yp];
+            ins=inpolygon(x,y,obj.x,obj.y);
+            xi = [xi x(ins)]';
+            yi = [yi y(ins)]';
+
         end
         
-        function in = inside(obj,x,y)
-            in = inpolygon(x,y,obj.x,obj.y);
-        end
     end
 end
